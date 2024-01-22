cfb2bca51a0390bc9b3a0bc30284165737e40adc
Robbyrush <rhogantech@gmail.com>
2024-01-21 21:13:36 -0600

    First JS file, console log and prompt

---
+++ main.js
@@ -0,0 +1,12 @@
+/*
+Name: Robert Hogan
+Date: 1/21/2024
+*/
+
+let basic = 5;
+basic = basic + 1;
+
+let i = prompt("What is your favorite tropical fruit to put on pizza?");
+
+console.log("var basic = " + basic);
+console.log("var i = " + i);
