e971ad76d1a6e357d828e577ee742b2600b35d42
Robbyrush <156552600+Robbyrush@users.noreply.github.com>
2024-04-07 19:22:36 -0500

    Update ArrayMethods.js
    - Fixed a bug

--- Unit 3/ArrayMethods.js
+++ Unit 3/ArrayMethods.js
@@ -29,7 +29,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 		var splitArray = input.value.split(" ");
 		splitArray.forEach(element => {
-			if (element.trim() !== "" {
+			if (element.trim() !== "") {
 				myArray.push(element);
 			}
 		});
