ddebffdd27d8321f2f4150acf9bc6a83f2297ef8
Robbyrush <156552600+Robbyrush@users.noreply.github.com>
2024-04-07 19:01:33 -0500

    Add files via upload

---
+++ Unit 3/ArrayMethods.js
@@ -0,0 +1,66 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+	// Variables
+	var myArray = [];
+	var input = document.getElementById("txtInput");
+	var p = document.getElementById("output");
+
+	// Adds click events to the buttons.
+	document.getElementById("btnPush").addEventListener("click", pushArray);
+	document.getElementById("btnPop").addEventListener("click", popArray);
+	document.getElementById("btnShift").addEventListener("click", shiftArray);
+	document.getElementById("btnUnshift").addEventListener("click", unshiftArray);
+	document.getElementById("btnMap").addEventListener("click", arrMap);
+
+	function update() 
+	{
+		p.innerHTML = "";
+
+		var myArrayR = myArray.reduce((total, value, index) => {
+			return total + (index + 1) + ". " + value + "<br>";
+		}, "");
+
+		p.innerHTML = myArrayR;
+	}
+
+	function pushArray() 
+	{
+		myArray = [];
+
+		var splitArray = input.value.split(" ");
+		splitArray.forEach(element => {
+			myArray.push(element);
+		});
+		update();
+	}
+
+	function popArray()
+	{
+		myArray.pop();
+		update();
+	}
+
+	function shiftArray()
+	{
+		myArray.shift();
+		update();
+	}
+
+	function unshiftArray()
+	{
+		myArray.unshift(input.value);
+		update();
+	}
+
+	function arrMap()
+	{
+		var arr = myArray.map((note) => {
+			return input.value + note;
+			// If you wanted it capitalized:
+			//return input.value.toUpperCase() + note;
+		});
+		myArray = arr;
+		update();
+	}
+
+});
